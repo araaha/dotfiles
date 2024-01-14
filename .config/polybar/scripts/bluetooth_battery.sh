ac0bfcb7ade02b3ba7ecbc04f3ffcffaad814c44
araaha <ara.ahady@gmail.com>
2024-01-14 09:20:19 -0500

    made changes to tdrop, cleaned up polybar

--- .config/polybar/scripts/bluetooth_battery.sh
+++ .config/polybar/scripts/bluetooth_battery.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-if dbus-send \
+if [ "$1" = "test" ]; then
+    dbus-send \
+        --print-reply=literal \
+        --system \
+        --dest=org.bluez /org/bluez/hci0/dev_40_C1_F6_2A_8C_DD \
+        org.freedesktop.DBus.Properties.Get \
+        string:"org.bluez.Battery1" \
+        string:"Percentage"
+elif dbus-send \
     --print-reply=literal \
     --system \
     --dest=org.bluez /org/bluez/hci0/dev_40_C1_F6_2A_8C_DD \
@@ -9,16 +17,15 @@ if dbus-send \
     string:"Percentage" > /dev/null 2>&1; then
 
     str=$(dbus-send \
-    --print-reply=literal \
-    --system \
-    --dest=org.bluez /org/bluez/hci0/dev_40_C1_F6_2A_8C_DD \
-    org.freedesktop.DBus.Properties.Get \
-    string:"org.bluez.Battery1" \
-    string:"Percentage" | tr -d -c 0-9)
+        --print-reply=literal \
+        --system \
+        --dest=org.bluez /org/bluez/hci0/dev_40_C1_F6_2A_8C_DD \
+        org.freedesktop.DBus.Properties.Get \
+        string:"org.bluez.Battery1" \
+        string:"Percentage" | tr -d -c 0-9)
 
     echo "$str"
 else 
-    echo "0"
     exit
-fi 
+fi
 
