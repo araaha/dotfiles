ea139762a20232d14ed219cccdefb14f25408849
araaha <ara.ahady@gmail.com>
2024-01-15 16:53:56 -0500

    removed redundancies

--- .local/share/applications/desktop-apps/reminder.md
+++
@@ -1,6 +0,0 @@
-add 
-
-## env GTK_USE_PORTAL=1
-
-to all desktop files that support xdg portal
-
--- misc/installer-scripts/enable-services.sh
+++
@@ -1,6 +0,0 @@
-systemctl enable --now cronie
-systemctl enable --now cups.socket
-systemctl enable --now udisks2.service
-systemctl enable --now ly
-systemctl enable --now --user greenclip
-systemctl enable --now --user redshift
