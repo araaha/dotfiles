b820f9155ab2afaaf1c385108cee19e5be554ab6
araaha <ara.ahady@gmail.com>
2024-01-28 15:15:14 -0500

    modified pacman.png

--- .local/share/xdg-filechooser/fzf-wrapper.sh
+++ .local/share/xdg-filechooser/fzf-wrapper.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 # This wrapper script is invoked by xdg-desktop-portal-termfilechooser.
 #
 # Inputs:
 # 1. "1" if multiple files can be chosen, "0" otherwise.
-# 2. "1" if a directory should be chosen, "0" otherwise.
+# 2. "1" if a directory bashould be chosen, "0" otherwise.
 # 3. "0" if opening files was requested, "1" if writing to a file was
 #    requested. For example, when uploading files in Firefox, this will be "0".
 #    When saving a web page in Firefox, this will be "1".
@@ -12,10 +12,10 @@
 #    path Firefox provided, such as "~/Downloads/webpage_title.html".
 #    Note that if the path already exists, we keep appending "_" to it until we
 #    get a path that does not exist.
-# 5. The output path, to which results should be written.
+# 5. The output path, to which results bashould be written.
 #
 # Output:
-# The script should print the selected paths to the output path (argument #5),
+# The script bashould print the selected paths to the output path (argument #5),
 # one path per line.
 # If nothing is printed, then the operation is assumed to have been canceled.
 
