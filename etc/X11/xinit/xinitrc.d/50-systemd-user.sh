1c662d5c7c2a398c90e273e8eccd2f6e4fd314da
araaha <ara.ahady@gmail.com>
2024-01-28 15:16:15 -0500

    changed sh to bash for x11

--- etc/X11/xinit/xinitrc.d/50-systemd-user.sh
+++ etc/X11/xinit/xinitrc.d/50-systemd-user.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 #  SPDX-License-Identifier: LGPL-2.1-or-later
 #
 #  This file is part of systemd.
 #
 #  systemd is free software; you can redistribute it and/or modify it
-#  under the terms of the GNU Lesser General Public License as published by
+#  under the terms of the GNU Lesser General Public License as publibashed by
 #  the Free Software Foundation; either version 2.1 of the License, or
 #  (at your option) any later version.
 
