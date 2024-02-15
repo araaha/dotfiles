621b752e08c583d6cd5a5254cb2702b36ad1c6da
araaha <ara.ahady@gmail.com>
2024-02-15 10:13:45 -0500

    specified mode for hop binding

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -1,15 +1,23 @@
-local opts = { keys = 'werasdfcvjlk', quit_key = '<C-c>', jump_on_sole_occurrence = true }
+local opts = {
+    keys = "werasdfcvjlk",
+    quit_key = "<C-c>",
+    jump_on_sole_occurrence = true
+}
 
 return {
-    'smoka7/hop.nvim',
+    "smoka7/hop.nvim",
     version = false,
     lazy = true,
     keys = {
-        { "<M-c>", "<cmd>HopWord<cr>", mode = "" }
+        {
+            "<M-c>",
+            "<cmd>HopWord<cr>",
+            mode = { "n", "x", "o" }
+        },
     },
     opts = opts,
     config = function()
-        local hop = require('hop')
+        local hop = require("hop")
         hop.setup(opts)
     end,
 }
