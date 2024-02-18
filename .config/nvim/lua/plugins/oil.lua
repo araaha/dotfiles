112d7b5d20ec2ca96a8e1e894c27d819d7b5c78e
araaha <ara.ahady@gmail.com>
2024-02-18 03:09:38 -0500

    added oil (file explorer) to plugins

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,28 @@
+return {
+    "stevearc/oil.nvim",
+    keys = {
+        { " o", "<cmd>Oil<CR>", mode = "n" }
+    },
+    config = function()
+        require("oil").setup({
+            keymaps = {
+                ["g?"] = "actions.show_help",
+                [">"] = "actions.select",
+                ["<"] = "actions.parent",
+                ["<C-s>"] = "actions.select_vsplit",
+                ["<C-h>"] = "actions.select_split",
+                ["<C-t>"] = "actions.select_tab",
+                ["<C-p>"] = "actions.preview",
+                ["<C-c>"] = "<Esc>",
+                ["X"] = "actions.close",
+                ["<C-l>"] = "actions.refresh",
+                ["`"] = "actions.cd",
+                ["~"] = "actions.tcd",
+                ["gs"] = "actions.change_sort",
+                ["gx"] = "actions.open_external",
+                ["g."] = "actions.toggle_hidden",
+                ["g\\"] = "actions.toggle_trash",
+            },
+        })
+    end
+}
