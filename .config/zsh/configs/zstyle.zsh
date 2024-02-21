35858ceccc078f5f44c9dd5c23419dabec9ec88a
araaha <ara.ahady@gmail.com>
2024-02-20 21:23:46 -0500

    modified zsh completion

--- .config/zsh/configs/zstyle.zsh
+++ .config/zsh/configs/zstyle.zsh
@@ -1,9 +1,7 @@
-zstyle ':completion:*' matcher-list '' \
-  'm:{a-z\-}={A-Z\_}' \
+zstyle ':completion:*' matcher-list 'm:{a-z\-}={A-Z\_}' \
   'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
   'r:|?=** m:{a-z\-}={A-Z\_}'
 
 zstyle ':completion:*' completer _complete _match
 zstyle ':completion:*' menu yes select
 zstyle ':completion:*' ignored-patterns '_*'
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
