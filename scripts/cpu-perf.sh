d5622883eacb8179197ad212f378f95aa0f6041b
araaha <ara.ahady@gmail.com>
2024-03-30 15:03:22 -0400

    renamed cpu_perf.sh to cpu-perf.sh

--- scripts/cpu-perf.sh
+++ scripts/cpu-perf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "guided"  | sudo tee /sys/devices/system/cpu/amd_pstate/status
 echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo "2500000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
