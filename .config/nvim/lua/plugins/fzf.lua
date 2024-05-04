8e89f6938dde5d441c42ef11b2fd7827cc99003e
araaha <ara.ahady@gmail.com>
2024-05-04 19:30:12 -0400

    cleaned up fzf.lua

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -7,9 +7,7 @@ return {
     config = function()
         local fzf = require("fzf-lua")
         local actions = require("fzf-lua").actions
-
         fzf.setup({
-            fzf_bin    = "fzf",
             keymap     = {
                 fzf = {
                     ["ctrl-u"] = "half-page-up"
@@ -27,6 +25,10 @@ return {
                 ["gutter"] = "-1",
                 ["bg"] = "-1",
             },
+            hls        = {
+                path_linenr = "GruvboxGreen",
+                buf_name = "FloatBorder"
+            },
             blines     = {
                 rg_opts = "--line-number --colors=line:none",
             },
@@ -34,7 +36,7 @@ return {
                 preview = {
                     default = "bat",
                     scrollbar = false,
-                    delay = 1,
+                    delay = 4,
                 },
                 win_border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
                 hl = { border = "FloatBorder", },
@@ -72,22 +74,10 @@ return {
             },
         })
 
+        vim.keymap.set("n", "<C-t>", fzf.files, {})
         vim.keymap.set("n", " fl", fzf.blines, {})
         vim.keymap.set("n", " ff", function()
             fzf.files({ cwd_prompt = false, cwd = "~" })
-        end, {})
-        vim.keymap.set("n", "<C-t>", function()
-            fzf.files({
-                preview = "bat --wrap=auto --style=plain --color=always --line-range=1:25 {} ",
-                actions = {
-                    ["default"] = actions.file_edit_or_qf,
-                    ["ctrl-x"] = actions.file_vsplit,
-                    ["ctrl-s"] = actions.file_split,
-                    ["ctrl-t"] = false,
-                    ["alt-q"] = actions.file_sel_to_qf,
-                    ["alt-l"] = actions.file_sel_to_ll,
-                }
-            })
         end, {})
     end,
 }
