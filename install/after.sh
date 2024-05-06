e3c8dfb9b8a0a61a9c5024d3d2657788ec4f0c2c
araaha <ara.ahady@gmail.com>
2024-05-06 17:12:21 -0400

    removed amd_iommu=off from grub
    might turn off laptop randomly

--- install/after.sh
+++ install/after.sh
@@ -23,13 +23,13 @@ echo "Defaults passwd_timeout=0" | sudo tee -a /etc/sudoers
 
 # Set up crontab
 crontab ~/dotfiles/misc/crontab/crontabs
-sudo crontab ~/dotfiles/misc/crontab/sudo-crontabs 
+sudo crontab ~/dotfiles/misc/crontab/sudo-crontabs
 
 # Change default shell to zsh
 chsh -s "$(which zsh)"
 
 # Configure GRUB settings
-sudo sed -i 's/GRUB_CMDLINE_LINUX="[^"]*"/GRUB_CMDLINE_LINUX="rootfstype=ext4 quiet splash amd_iommu=off atkbd.softrepeat=1 vt.cur_default=0x200011 vt.color=3 vt.global_cursor_default=0 cpufreq.scaling_min_freq=2500000 cpufreq.default_governor=powersave amd_pstate=guided"/' /etc/default/grub
+sudo sed -i 's/GRUB_CMDLINE_LINUX="[^"]*"/GRUB_CMDLINE_LINUX="rootfstype=ext4 quiet splash atkbd.softrepeat=1 vt.cur_default=0x200011 vt.color=3 vt.global_cursor_default=0 cpufreq.scaling_min_freq=2500000 cpufreq.default_governor=powersave amd_pstate=guided"/' /etc/default/grub
 sudo cp -v ~/.local/share/Wallpapers/Pacman.png /boot/grub/
 sudo sed -i 's/GRUB_BACKGROUND="[^"]*"/GRUB_BACKGROUND="/boot/grub/Pacman.png"/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
