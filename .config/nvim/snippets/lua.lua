02f8161197ae66159cc1330371ba7942744dad36
araaha <ara.ahady@gmail.com>
2024-07-15 00:25:26 -0400

    added lua snippets

--- .config/nvim/snippets/lua.lua
+++ .config/nvim/snippets/lua.lua
@@ -1,26 +1,25 @@
 local ls = require("luasnip")
 local s = ls.s
 local i = ls.i
-local t = ls.t
+-- local t = ls.t
 
-local d = ls.dynamic_node
-local c = ls.choice_node
-local f = ls.function_node
-local sn = ls.snippet_node
+-- local d = ls.dynamic_node
+-- local c = ls.choice_node
+-- local f = ls.function_node
+-- local sn = ls.snippet_node
 
 local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
-local rep = require("luasnip.extras").rep
+-- local rep = require("luasnip.extras").rep
 
-local snippets = {}
-
-local plug = s(
-    {
-        trig = "plug",
-        regTrig = false,
-        snippetType = "autosnippet",
-        hidden = true,
-    }, fmt([[
+return {
+    s(
+        {
+            trig = "plug",
+            regTrig = false,
+            snippetType = "autosnippet",
+            hidden = true,
+        }, fmt([[
     return {{
         "{}",
         version = {},
@@ -29,14 +28,33 @@ local plug = s(
         opts    = {{}},
     }}
 ]], {
-        i(1, ""),
-        i(2, "false"),
-        i(3, "true"),
-        i(4, "\"VeryLazy\""),
-    })
-)
-
-table.insert(snippets, plug)
-
-
-return snippets
+            i(1, ""),
+            i(2, "false"),
+            i(3, "true"),
+            i(4, "\"VeryLazy\""),
+        })
+    ),
+    s(
+        {
+            trig = "snip",
+            regTrig = false,
+            snippetType = "snippet",
+            hidden = false,
+        }, fmta([[
+        s(
+            {
+                trig = "<>",
+                snippetType = "<>",
+                hidden = <>,
+            }, fmta("<>", {
+                <>
+            })
+        ),]], {
+            i(1),
+            i(2),
+            i(3),
+            i(4),
+            i(5)
+        })
+    ),
+}
