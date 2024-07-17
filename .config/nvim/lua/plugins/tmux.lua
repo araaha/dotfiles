420184dcef976e3d75127b5fa0f30a08aed62688
araaha <ara.ahady@gmail.com>
2024-07-17 01:00:47 -0400

    replaced navigator.lua with tmux.lua

---
+++ .config/nvim/lua/plugins/tmux.lua
@@ -0,0 +1,19 @@
+return {
+    "alexghergh/nvim-tmux-navigation",
+    version = false,
+    lazy    = true,
+    keys    = {
+        "<M-h>",
+        "<M-j>",
+        "<M-k>",
+        "<M-l>"
+    },
+    opts    = {
+        keybindings = {
+            left = "<M-h>",
+            down = "<M-j>",
+            up = "<M-k>",
+            right = "<M-l>",
+        }
+    }
+}
