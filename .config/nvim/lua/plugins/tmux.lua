eba6ea41344647648999f931f7ffae7d33af6932
araaha <ara.ahady@gmail.com>
2024-07-19 21:09:22 -0400

    replaced tmux plugin

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -1,19 +1,18 @@
 return {
-    "alexghergh/nvim-tmux-navigation",
+    "nathom/tmux.nvim",
     version = false,
-    lazy    = true,
     keys    = {
         "<M-h>",
         "<M-j>",
         "<M-k>",
         "<M-l>"
     },
-    opts    = {
-        keybindings = {
-            left = "<M-h>",
-            down = "<M-j>",
-            up = "<M-k>",
-            right = "<M-l>",
-        }
-    }
+    config  = function()
+        local tmux = require("tmux")
+        local map = vim.keymap.set
+        map("", "<M-h>", function() tmux.move_left() end, {})
+        map("", "<M-j>", function() tmux.move_down() end, {})
+        map("", "<M-k>", function() tmux.move_up() end, {})
+        map("", "<M-l>", function() tmux.move_right() end, {})
+    end
 }
