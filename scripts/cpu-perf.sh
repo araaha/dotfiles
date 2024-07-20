f0b053131e88a1259b5101c0e43d7832573e162b
araaha <ara.ahady@gmail.com>
2024-07-20 17:24:07 -0400

    lower scaling_min_freq

--- scripts/cpu-perf.sh
+++ scripts/cpu-perf.sh
@@ -2,4 +2,4 @@
 
 echo "guided"  | sudo tee /sys/devices/system/cpu/amd_pstate/status
 echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
-echo "2500000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
+echo "1800000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq
