1ea57f3dd84f31b1e93c1e67c884036619a07e2c
araaha <ara.ahady@gmail.com>
2024-08-09 19:40:15 -0400

    tree-pairs now lazyloaded

--- .config/nvim/lua/plugins/treepairs.lua
+++ .config/nvim/lua/plugins/treepairs.lua
@@ -1,5 +1,5 @@
 return {
-    "yorickpeterse/nvim-tree-pairs",
-    commit = "edc0eb062db1eb4ec520f05c7ef2798d33dcab64",
-    event = "VeryLazy"
+    "araaha/nvim-tree-pairs",
+    opts = {},
+    keys = "%",
 }
