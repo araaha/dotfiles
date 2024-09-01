702bd4d247e54b6d15e07bbe598f8c2b12bf177a
araaha <ara.ahady@gmail.com>
2024-09-01 18:49:30 -0400

    Modified xbacklight.sh

--- .config/polybar/scripts/xbacklight.sh
+++ .config/polybar/scripts/xbacklight.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-bright=$(light -G)
-compare="$(echo $bright'<'100 | bc -l)"
-single="$(echo $bright'<'10 | bc -l)"
-if [[ "$compare" == "1" && "$single" == "0" ]];
-then 
-    echo $bright | grep -Po "^.."
-elif [[ "$compare" == "1" && "$single" == "1" ]]; 
-then
-    echo $bright | grep -Po "^."
-else
-    echo $bright | grep -Po "^..."
-fi
+while true; do
+    printf "%.0f\n" "$(light -G)"
+    sleep 0.01
+done
