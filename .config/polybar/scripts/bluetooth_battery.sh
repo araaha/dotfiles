f30bbc427dfb516877903e6857cee802bfd9e76d
araaha <ara.ahady@gmail.com>
2024-09-01 18:48:52 -0400

    Modified bluetooth_battery

--- .config/polybar/scripts/bluetooth_battery.sh
+++ .config/polybar/scripts/bluetooth_battery.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
+# Execute the dbus-send command and capture stderr
+DBUS_OUTPUT=$(dbus-send \
+        --print-reply=literal \
+        --system \
+        --dest=org.bluez /org/bluez/hci0/dev_40_C1_F6_2A_8C_DD \
+        org.freedesktop.DBus.Properties.Get \
+        string:"org.bluez.Battery1" \
+        string:"Percentage")
+DBUS_EXIT_STATUS=$?
+
 if [ "$1" = "test" ]; then
-    dbus-send \
-        --print-reply=literal \
-        --system \
-        --dest=org.bluez /org/bluez/hci0/dev_40_C1_F6_2A_8C_DD \
-        org.freedesktop.DBus.Properties.Get \
-        string:"org.bluez.Battery1" \
-        string:"Percentage"
-elif dbus-send \
-    --print-reply=literal \
-    --system \
-    --dest=org.bluez /org/bluez/hci0/dev_40_C1_F6_2A_8C_DD \
-    org.freedesktop.DBus.Properties.Get \
-    string:"org.bluez.Battery1" \
-    string:"Percentage" > /dev/null 2>&1; then
-
-    str=$(dbus-send \
-        --print-reply=literal \
-        --system \
-        --dest=org.bluez /org/bluez/hci0/dev_40_C1_F6_2A_8C_DD \
-        org.freedesktop.DBus.Properties.Get \
-        string:"org.bluez.Battery1" \
-        string:"Percentage" | tr -d -c 0-9)
-
-    echo "$str"
-else 
-    exit
+    if [ -n "$DBUS_OUTPUT" ]; then
+        exit 0
+    fi
+    exit 1
 fi
 
+if [ $DBUS_EXIT_STATUS -eq 0 ]; then
+    percent=$(echo "$DBUS_OUTPUT" | tr -d -c 0-9)
+    echo "$percent%"
+else
+    exit 1
+fi
