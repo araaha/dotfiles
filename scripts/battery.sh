55e4857bb62c2ed8861eb34294b17720cba540a8
araaha <ara.ahady@gmail.com>
2024-11-24 20:54:20 -0500

    Modified battery, bluetooth-battery scripts
    - Now, scripts don't require specific MAC address or battery type. Works
    regardless of setup

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -3,9 +3,11 @@
 session_address=$(grep -E -z "DBUS_SESSION_BUS_ADDRESS" /proc/$(pgrep -u "$LOGNAME" openbox)/environ | tr -d '\0')
 eval "export $session_address"
 
-battery_level="$(cat /sys/class/power_supply/BATT/capacity)"
+battery_name="$(upower -e | rg 'battery' | cut -d '_' -f 2)"
 
-charging_status="$(upower -i /org/freedesktop/UPower/devices/battery_BATT | grep 'state' | awk '{print $2}')"
+battery_level="$(cat /sys/class/power_supply/"$battery_name"/capacity)"
+
+charging_status="$(upower -i /org/freedesktop/UPower/devices/battery_"$battery_name" | grep 'state' | awk '{print $2}')"
 
 if [ "$battery_level" -le 10 ] && [ "$charging_status" != "charging" ]
 then
--- scripts/bluetooth-battery.sh
+++ scripts/bluetooth-battery.sh
@@ -3,11 +3,16 @@
 session_address=$(grep -E -z "DBUS_SESSION_BUS_ADDRESS" /proc/$(pgrep -u "$LOGNAME" openbox)/environ | tr -d '\0')
 eval "export $session_address"
 
-bluetooth_percent=$(upower -i /org/freedesktop/UPower/devices/headset_dev_40_C1_F6_2A_8C_DD | rg percentage | awk '{print $2}')
+headset=$(upower -e | rg 'headset' | cut -d '_' -f 3- )
+
+MAC=$(echo "$headset" | tr _ :)
+connected=$(bluetoothctl info "$MAC" | rg 'Connected' | cut -d " " -f 2)
+
+bluetooth_percent=$(upower -i /org/freedesktop/UPower/devices/headset_dev_"$headset" | rg percentage | awk '{print $2}')
 
 bluetooth_battery_num="${bluetooth_percent%?}"
 
-if [ "$bluetooth_battery_num" -eq 10 ]; then
+if [ "$bluetooth_battery_num" -eq 10 ] && [ "$connected" = "yes" ]; then
     DISPLAY=:0 dunstify -u CRITICAL -t 1000 "Headset at 10%"
 fi
 
