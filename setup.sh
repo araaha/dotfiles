60ce540bc8273e33ca90cd054a842bfa154e29d4
araaha <ara.ahady@gmail.com>
2024-11-29 02:00:39 -0500

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-shopt -s extglob
-
-echo "run shopt -s extglob first"
-sleep 3
-
 bash -c /home/araaha/dotfiles/install/lncp.sh
 bash -c /home/araaha/dotfiles/install/yay.sh
 bash -c /home/araaha/dotfiles/install/after.sh
