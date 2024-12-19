2dcf5b483af360273581a78a99f22b648116facd
araaha <ara.ahady@gmail.com>
2024-12-19 01:16:15 -0500

    Fixed fzf-wrapper so that absolute file-path is not shown

--- .local/share/xdg-filechooser/fzf-wrapper.sh
+++ .local/share/xdg-filechooser/fzf-wrapper.sh
@@ -18,7 +18,7 @@
 # The script bashould print the selected paths to the output path (argument #5),
 # one path per line.
 # If nothing is printed, then the operation is assumed to have been canceled.
-export FZF_DEFAULT_OPTS="--no-scrollbar --border=none --no-separator  --tiebreak=length,end,chunk --padding 0% --margin 0% --multi --reverse --preview-window=border-none --color=bg+:-1,spinner:#fb4934,hl:#928374:bold,fg:#8abeb7,header:#928374,info:#8ec07c,pointer:#9cd365,marker:#fb4934,fg+:regular:reverse:#83a598,prompt:#9cd365,hl+:reverse:reverse:#83a598,gutter:-1,query:regular,border:#87afaf --bind home:first --bind end:last --bind ctrl-p:up --bind ctrl-d:half-page-down --bind ctrl-u:half-page-up --bind 'ctrl-l:become(lf {})' --bind 'ctrl-y:offset-up' --bind 'ctrl-e:offset-down' --bind 'ctrl-a:select-all'"
+export FZF_DEFAULT_OPTS="--delimiter=/ --with-nth 4.. --no-scrollbar --border=none --no-separator  --tiebreak=length,end,chunk --padding 0% --margin 0% --multi --reverse --preview-window=border-none --color=bg+:-1,spinner:#fb4934,hl:#928374:bold,fg:#8abeb7,header:#928374,info:#8ec07c,pointer:#9cd365,marker:#fb4934,fg+:regular:reverse:#83a598,prompt:#9cd365,hl+:reverse:reverse:#83a598,gutter:-1,query:regular,border:#87afaf --bind home:first --bind end:last --bind ctrl-p:up --bind ctrl-d:half-page-down --bind ctrl-u:half-page-up --bind 'ctrl-l:become(lf {})' --bind 'ctrl-y:offset-up' --bind 'ctrl-e:offset-down' --bind 'ctrl-a:select-all'"
 
 export FD_EXCLUDE="-E '.cache' -E 'icons' -E 'themes' -E '**pkg' -E '**.git' -E 'state' -E 'google-chrome' -E 'opt' -E 'chromium' -E 'firefox' -E 'cargo'"
 
@@ -43,8 +43,3 @@ fi
 if ! [ -s "$out" ]; then
     exit 1
 fi
-
-# file_path=$(cat "$out")
-# full_path="$HOME/$file_path"
-#
-# echo "$full_path" > "$out"
