48b168c04ab02c8a5319d7f451f10a6860bb9e04
araaha <ara.ahady@gmail.com>
2025-06-06 22:22:39 -0400

    Modified nvim options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -25,7 +25,6 @@ local options = {
     splitbelow    = true,
     pumheight     = math.floor(vim.o.lines * 0.33),
     shortmess     = "aIF",
-    scrolloff     = 999,
     commentstring = "#%s",
     grepprg       = "rg --color=auto --vimgrep",
     grepformat    = "%f:%l:%c:%m",
