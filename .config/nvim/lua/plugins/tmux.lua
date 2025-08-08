2630b7691966c51f6aa5a4c519c31d3c903c1f64
araaha <ara.ahady@gmail.com>
2025-08-08 11:08:09 -0400

    Simplified tmux.lua keymap

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -4,62 +4,42 @@ return {
         {
             "<M-h>",
             "<cmd>lua require('tmux').move_left()<cr>",
-            mode = { "n" }
+            mode = { "n", "t" }
         },
         {
             "<M-l>",
             "<cmd>lua require('tmux').move_right()<cr>",
-            mode = { "n" }
+            mode = { "n", "t" }
         },
         {
             "<M-j>",
             "<cmd>lua require('tmux').move_down()<cr>",
-            mode = { "n" }
+            mode = { "n", "t" }
         },
         {
             "<M-k>",
             "<cmd>lua require('tmux').move_up()<cr>",
-            mode = { "n" }
-        },
-        {
-            "<M-h>",
-            "<cmd>lua require('tmux').move_left()<cr>",
-            mode = { "t" }
-        },
-        {
-            "<M-l>",
-            "<cmd>lua require('tmux').move_right()<cr>",
-            mode = { "t" }
-        },
-        {
-            "<M-j>",
-            "<cmd>lua require('tmux').move_down()<cr>",
-            mode = { "t" }
-        },
-        {
-            "<M-k>",
-            "<cmd>lua require('tmux').move_up()<cr>",
-            mode = { "t" }
+            mode = { "n", "t" }
         },
         {
             "<M-h>",
             "<C-o><cmd>lua require('tmux').move_left()<cr>",
-            mode = "i"
+            mode = { "i" }
         },
         {
             "<M-j>",
             "<C-o><cmd>lua require('tmux').move_down()<cr>",
-            mode = "i"
+            mode = { "i" }
         },
         {
             "<M-k>",
             "<C-o><cmd>lua require('tmux').move_up()<cr>",
-            mode = "i"
+            mode = { "i" }
         },
         {
             "<M-l>",
             "<C-o><cmd>lua require('tmux').move_right()<cr>",
-            mode = "i"
+            mode = { "i" }
         },
     }
 }
