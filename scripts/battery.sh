a602a5c3a75202538b3af6adbe3864d654b77b82
araaha <ara.ahady@gmail.com>
2025-08-17 20:01:19 -0400

    Modified battery scripts
    - removed session_address

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-session_address=$(grep -E -z "DBUS_SESSION_BUS_ADDRESS" /proc/$(pgrep -u "$LOGNAME" openbox)/environ | tr -d '\0')
-eval "export $session_address"
-
 battery_name="$(upower -e | rg 'battery' | cut -d '_' -f 2)"
 
 battery_level="$(cat /sys/class/power_supply/"$battery_name"/capacity)"
