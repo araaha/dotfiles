9b8704d47749c2843c5fc900271d977320a625b9
araaha <ara.ahady@gmail.com>
2025-08-18 23:58:44 -0400

    Modified lncp.sh
    - symlinks now verbose
    - fixed themes

--- install/lncp.sh
+++ install/lncp.sh
@@ -16,15 +16,15 @@ mkdir -p "$HOME/Screenshots/"
 mkdir -p "$HOME/.local/share/themes"
 mkdir -p "$HOME/.local/share/icons"
 
-ln -sr "$DOTS/scripts/" "$HOME/"
-ln -srf "$DOTS/.config"/* "$HOME/.config/"
-find "$DOTS/.local/share" -mindepth 1 -maxdepth 1 ! -name "themes" -exec ln -sr {} "$HOME/.local/share" \;
+ln -srv "$DOTS/scripts/" "$HOME/"
+ln -srfv "$DOTS/.config"/* "$HOME/.config/"
+find "$DOTS/.local/share" -mindepth 1 -maxdepth 1 ! -name "themes" -exec ln -srv {} "$HOME/.local/share" \;
 
 #themes/icons
 yay -S unzip
 
-unzip "$DOTS/.local/share/themes/Gruvbox-Material-Dark-HIDPI.zip" -d "$HOME/.local/share/themes/Gruvbox-Material-Dark-HIDPI.zip"
-unzip "$DOTS/.local/share/themes/gruvbox-material-dark-blocks.zip" -d "$HOME/.local/share/themes/gruvbox-material-dark-blocks.zip"
+unzip "$DOTS/.local/share/themes/Gruvbox-Material-Dark-HIDPI.zip" -d "$HOME/.local/share/themes/"
+unzip "$DOTS/.local/share/themes/gruvbox-material-dark-blocks.zip" -d "$HOME/.local/share/themes/"
 
 git clone "https://github.com/SylEleuth/gruvbox-plus-icon-pack" ~/.local/share/icons
 
@@ -32,7 +32,7 @@ find ~/.local/share/icons/ -mindepth 1 -maxdepth 1 ! -name "Gruvbox-Plus-Dark" -
 
 #binaries
 mkdir -p ~/.local/bin
-find "$HOME/dotfiles/.local/bin" -type f -executable -print0 | xargs -0 -I % ln -sr % ~/.local/bin
+find "$HOME/dotfiles/.local/bin" -type f -executable -print0 | xargs -0 -I % ln -srv % ~/.local/bin
 
 #/etc/
 sudo cp -rv "$DOTS/etc"/* /etc/
