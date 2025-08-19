bc0332676fdde5870fd466f306cf2a9ef7fcd587
araaha <ara.ahady@gmail.com>
2025-08-18 23:58:58 -0400

    Modified yay.sh
    - removed unnecessary commands

--- install/yay.sh
+++ install/yay.sh
@@ -2,8 +2,6 @@
 
 #set up yay
 yay -Sy
-yay -Y --gendb
-yay -Y --devel --save
 
 #Install apps
 yay --disable-download-timeout --removemake --needed --sudoloop -S - < ~/dotfiles/install/arch-apps.txt
