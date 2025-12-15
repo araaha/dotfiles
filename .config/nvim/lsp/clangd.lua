59c1741bc7110edbcafc126c4bdee438659f9747
araaha <ara.ahady@gmail.com>
2025-12-14 20:39:27 -0500

    Modified clangd.lua

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -1,5 +1,8 @@
 return {
-    cmd = { "clangd" },
+    cmd = {
+        "clangd",
+        "--function-arg-placeholders=1",
+    },
     filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
     root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac", ".git" },
     capabilities = {
