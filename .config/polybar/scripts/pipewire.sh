4f45cc4d09e683973272f7aec629f8f600f98ad3
araaha <ara.ahady@gmail.com>
2026-04-06 23:59:56 -0400

    Modified polybar volume module

--- .config/polybar/scripts/pipewire.sh
+++ .config/polybar/scripts/pipewire.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 while true; do
     VOLUME=$(pamixer --get-volume)
+    ROUNDED_VOLUME=$((VOLUME /2 *2))
     IS_MUTED=$(pamixer --get-volume-human)
     if [ "$IS_MUTED" = "muted" ]; then
-        echo "󰝟 $VOLUME%"
+        echo "󰝟 $ROUNDED_VOLUME%"
     else
-        echo "󰕾 $VOLUME%"
+        echo "󰕾 $ROUNDED_VOLUME%"
     fi
     sleep 0.01
 done
