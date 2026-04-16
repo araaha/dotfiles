dff78e5f78907c72394ace764fbecad8e46c7107
araaha <ara.ahady@gmail.com>
2026-04-16 13:17:17 -0400

    Added tiny-cmdline (nvim 0.12+)

---
+++ .config/nvim/lua/plugins/cmdline.lua
@@ -0,0 +1,8 @@
+return {
+    "rachartier/tiny-cmdline.nvim",
+    event = "VeryLazy",
+    opts = {
+        border = { "┏", "━", "┓", "┃", "┛", "━", "┗", "┃" },
+        menu_col_offset = 4,
+    }
+}
