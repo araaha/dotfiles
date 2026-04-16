dd389a7e73a0e5c6322d4e6161dc9d663f4fa370
araaha <ara.ahady@gmail.com>
2026-04-16 13:13:24 -0400

    Modified polybar brightness script

--- .config/polybar/scripts/xbacklight.sh
+++ .config/polybar/scripts/xbacklight.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 while true; do
-    printf "%.0f\n" "$(light -G)"
+    LIGHT=$(light -G)
+    ROUNDED_VOLUME=$(printf "%.0f" "$LIGHT")
+    printf "%d\n" $((ROUNDED_VOLUME / 2 * 2))
     sleep 1
 done
