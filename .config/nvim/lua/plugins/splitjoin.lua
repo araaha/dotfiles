162ad818573be65a8fbcebf24e1da2312eebe5e1
araaha <ara.ahady@gmail.com>
2026-08-25 15:07:30 -0400

    feat(nvim): added splitjoin

---
+++ .config/nvim/lua/plugins/splitjoin.lua
@@ -0,0 +1,11 @@
+return {
+    "nvim-mini/mini.splitjoin",
+    keys = { "gS" },
+    opts = {
+        mappings = {
+            toggle = 'gS',
+            split = '',
+            join = '',
+        },
+    },
+}
